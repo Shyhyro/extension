a74fec9b0a0921a32f6f2f93d29796a10cba6582
shay <43248357+shayypy@users.noreply.github.com>
2021-07-24 13:10:01 -0500

    Rewrite `mergeImage` and `init`, remove 1.0 methods

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
-const regexPattern = /^https:\/\/(v2\.|www\.)?horsereality\.(com|nl)\/horses\/(\d{3,10})\/.*/
+const hrUrlRegex = /^https:\/\/(v2\.|www\.)?horsereality\.(com|nl)\/horses\/(\d{1,10})\/.*/
 
-async function init() {
-    const currentTabs = await chrome.tabs.query({active: true, currentWindow: true});
-    const currentTab = currentTabs[0];
-    const currentUrl = currentTab.url;
+function init() {
+    const head = document.getElementsByTagName('head')[0];
+    const stylesheet = document.createElement('link');
+    stylesheet.rel = 'stylesheet';
+    stylesheet.href = 'https://realmerge.shay.cat/static/extensions.css';
+    head.appendChild(stylesheet);
 
-    if (!regexPattern.test(currentUrl)) {
-        document.getElementById('error-invalid-url').style.display = 'block';
-        const body = document.getElementsByTagName("body")[0];
-        body.innerHTML = '<p style="padding:10px;margin:0">This page is not a valid horse page. <a href="https://realmerge.shay.cat" target="_blank">Visit the main website here</a>.</p>';
-        body.style.height = '20px';
-        body.style.width = '230px';
-        return
-    } else {
-        await mergeImage(currentUrl);
-        return
-    }
+    const button = document.createElement('button');
+    button.classList = 'yellow';
+    button.id = 'realmerge-merge-button';
+    button.title = 'with Realmerge! :)';
+    button.onclick = () => {mergeImage()};
+    const text = document.createTextNode('Merge');
+    button.appendChild(text);
+
+    const pane = document.getElementsByClassName('horse_left')[0];
+    pane.insertBefore(button, pane.children[1]);
 }
 init();
 
-function displayError(message) {
-    document.getElementById('error-message').innerHTML = message;
-    document.getElementById('error-box').style.display = 'block';
-    document.getElementById('merged-image-box').style.display = 'none';
-}
+async function mergeImage() {
+    const url = window.location.href;
+    if (!hrUrlRegex.test(url)) return;
 
-async function mergeImage(url) {
-    // basic UX in case the request takes a long time
-    document.getElementById('merged-image-title').innerHTML = 'Processing...';
-    document.getElementById('merged-image').src = '';
-
-    document.getElementById('merged-image-box').style.display = 'block';
-    document.getElementById('merged-image-title').innerHTML = 'Merging...';
-
+    // merge the horse
     const response = await fetch('https://realmerge.shay.cat/api/merge', {
         method: 'POST',
-        body: JSON.stringify({url: url}),
+        body: JSON.stringify({url: url, return_layer_urls: true}),
         headers: {'Content-Type': 'application/json'}
     });
     const data = await response.json();
-    if (!response.ok) {
-        displayError(data.message);
-        return
-    } else {
-        document.getElementById('error-box').style.display = 'none';
-        document.getElementById('merged-image-box').style.display = 'block';
-        // it might be hidden if the previous
-        // request resulted in an error
-    };
 
-    document.getElementById('merged-image').src = data.url;
-    if (!data.name) {
-        document.getElementById('merged-image-title').innerHTML = 'Merged Image';
-    } else {
-        document.getElementById('merged-image-title').innerHTML = data.name;
+    // error-from-server handling
+    if (!response.ok) {
+        alert(`Realmerge error: ${data.message}`);
+        return
     }
+    // remove the top bar, realmerge is more important :sunglasses:
+    const looking_at = document.getElementsByClassName('looking_at')[0]
+    if (looking_at) looking_at.remove();
+
+    // add our layers (replaces the top bar)
+    const layersBox = document.createElement('div');
+    layersBox.classList = 'realmerge-layers-slider looking_at';
+    for (const layerImgUrl of data.layer_urls) {
+        const layerImg = document.createElement('img');
+        layerImg.src = layerImgUrl;
+        layersBox.appendChild(layerImg);
+    }
+    const horse_banner = document.getElementsByClassName('horse_banner')[0];
+    horse_banner.insertBefore(layersBox, horse_banner.children[0]);
+
+    // show the merged horse
+    function replaceWithMerged(parent, mergedUrl, foal=false) {
+        while (parent.lastChild) {
+            parent.removeChild(parent.lastChild);
+        }
+        const merged = document.createElement('img');
+        merged.src = mergedUrl;
+        if (foal) merged.classList = 'realmerge-merged-result foal';
+        else merged.classList = 'realmerge-merged-result';
+        parent.appendChild(merged);
+    }
+
+    // remove the button, you only need to merge once silly
+    const mergeButton = document.querySelector('#realmerge-merge-button');
+    if (mergeButton) mergeButton.remove();
 }
